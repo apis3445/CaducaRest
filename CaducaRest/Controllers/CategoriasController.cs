1d57b653caf8af4e2679e3f48f7f4fdb8491a517
abi1521@hotmail.com <abi1521@hotmail.com>
2018-10-08 23:20:07 -0500

    Servicio para categorías y Scripts para crear base de datos

---
+++ CaducaRest/Controllers/CategoriasController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CaducaRest.Models;
+
+namespace CaducaRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly CaducaContext _context;
+
+        public CategoriasController(CaducaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Categorias
+        [HttpGet]
+        public IEnumerable<Categoria> GetCategoria()
+        {
+            return _context.Categoria;
+        }
+
+        // GET: api/Categorias/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCategoria([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var categoria = await _context.Categoria.FindAsync(id);
+
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(categoria);
+        }
+
+        // PUT: api/Categorias/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategoria([FromRoute] int id, [FromBody] Categoria categoria)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != categoria.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(categoria).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoriaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Categorias
+        [HttpPost]
+        public async Task<IActionResult> PostCategoria([FromBody] Categoria categoria)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Categoria.Add(categoria);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCategoria", new { id = categoria.Id }, categoria);
+        }
+
+        // DELETE: api/Categorias/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategoria([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var categoria = await _context.Categoria.FindAsync(id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categoria.Remove(categoria);
+            await _context.SaveChangesAsync();
+
+            return Ok(categoria);
+        }
+
+        private bool CategoriaExists(int id)
+        {
+            return _context.Categoria.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ CaducaRest/Scripts/CaducaBD.sql
@@ -0,0 +1,5 @@
+﻿CREATE DATABASE caduca CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER 'AdminCaduca'@'localhost' IDENTIFIED BY 'StKRV6MR6A'; 
+GRANT ALL PRIVILEGES ON caduca.* TO 'AdminCaduca'@'localhost';
+CREATE USER 'SistemaCaduca'@'localhost' IDENTIFIED BY 'xADcUaP5cs';
+GRANT SELECT,INSERT,UPDATE,DELETE ON caltic.* TO 'SistemaCaduca'@'localhost';

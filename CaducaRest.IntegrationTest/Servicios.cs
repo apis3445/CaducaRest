808bd345dbb75969736c7fd9c15b293d0000f240
abi <abi1521@hotmail.com>
2019-11-10 21:05:41 -0600

    SQLite

--- CaducaRest.IntegrationTest/Servicios.cs
+++ CaducaRest.IntegrationTest/Servicios.cs
@@ -2,12 +2,8 @@
 using CaducaRest.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
-using Microsoft.Data.Sqlite;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
@@ -39,9 +35,7 @@ namespace CaducaRest.IntegrationTest
             //Esto nos crea un servidor con los servicios rest 
             var servidorPruebas = new TestServer(builder);
             //Obtenemos el objeto caducaContext
-            caducaContext = servidorPruebas.Host.Services.GetService(typeof(CaducaContext)) as CaducaContext;
-            //Abrimos la conexión
-           
+            caducaContext = servidorPruebas.Host.Services.GetService(typeof(CaducaContext)) as CaducaContext;            
             //Inicializamos la bd con datos de prueba
             InicializaDatos.Inicializar(caducaContext);
             //var total =caducaContext.Usuario.Count();

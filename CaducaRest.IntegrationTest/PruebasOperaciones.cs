1d201338a85dc833759756fc6712c2c2626028f0
abi <abi1521@hotmail.com>
2019-11-10 21:14:01 -0600

    DataRow

---
+++ CaducaRest.IntegrationTest/PruebasOperaciones.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CaducaRest.IntegrationTest
+{
+    [TestClass]
+    public class PruebasOperaciones
+    {
+        [TestMethod]
+        [DataRow(1,3,4)]
+        [DataRow(2,7,9)]
+        public void SumaDosNumeros_Correcto(int a, int b, int total)
+        {
+            Operaciones operaciones = new Operaciones(a, b);
+            int resultado = operaciones.Sumar();
+            Assert.AreEqual(total, resultado);
+        }
+    }
+}

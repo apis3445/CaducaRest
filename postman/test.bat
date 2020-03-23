2ff3b36c8feda0cbe13c02229e8da64b24b696a8
apis3445 <abigail_armijo@yahoo.com>
2020-03-23 11:45:40 -0600

    Update test.bat

--- postman/test.bat
+++ postman/test.bat
@@ -1,1 +1,2 @@
 newman run test.postman_collection.json -x --reporters cli,junit --reporter-junit-export Results\junitReport.xml
+newman run test.postman_collection.json -x --reporters cli,junit --reporter-junit-export Results\junitReport2.xml

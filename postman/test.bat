2f1c9df1775b55870a1b193ca86a2d02c4a62ab8
apis3445 <abigail_armijo@yahoo.com>
2020-06-30 22:37:13 -0500

    Update test.bat

--- postman/test.bat
+++ postman/test.bat
@@ -1,5 +1,5 @@
-call newman run PostmanTesting.postman_collection.json --reporters cli,junit Results\PostmanJunit.xml
-call newman run test.postman_collection.json  -r htmlextra --reporters cli,junit --reporter-junit-export Results\junitReport2.xml 
+call newman run PostmanTesting.postman_collection.json -x --reporters junit Results\PostmanJunit.xml
+call newman run test.postman_collection.json -x -r htmlextra --reporters junit --reporter-junit-export Results\junitReport2.xml 
 call newman run Login/Login.postman_collection.json -e -x Login/Login.Azure.postman_environment.json -d Login/login.data.csv -r junitfull --reporter-junitfull-export Results/junitReport3.xml -n 2
 call newman run Login/Login.postman_collection.json -e -x Login/Login.Azure.postman_environment.json -d Login/login.data.csv -r htmlextra --reporter-htmlextra-skipSensitiveData --reporter-htmlextra-showMarkdownLinks Results/report.html
-call newman run Login/Login.postman_collection.json -e -x Login/Login.Azure.postman_environment.json -d Login/login.data.csv --reporters cli,junit Results\LoginJunit.xml
+call newman run Login/Login.postman_collection.json -e -x Login/Login.Azure.postman_environment.json -d Login/login.data.csv --reporters junit Results\LoginJunit.xml

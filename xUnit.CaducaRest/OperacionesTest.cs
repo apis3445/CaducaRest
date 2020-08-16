555c81aceb2b203e4854edf897fcb9f671fb5b53
apis3445 <abigail_armijo@yahoo.com>
2020-08-15 22:22:22 -0500

    Update OperacionesTest.cs

--- xUnit.CaducaRest/OperacionesTest.cs
+++ xUnit.CaducaRest/OperacionesTest.cs
@@ -5,12 +5,15 @@ namespace xUnit.CaducaRest
     public class OperacionesTest
     { 
         [Fact]
-        public void SumaDosNumeros_Correcto()
+        public void Operaciones_SumaDosNumeros_RegresaLaSuma()
         {
+            //Inicialización de datos (Arrange)
             int a = 1;
             int b=3;
             Operaciones operaciones = new Operaciones(a,b);
+            //Método a probar (Act)
             int resultado = operaciones.Sumar();
+            //Comprobación de resultados (Assert)
             Assert.Equal(4, resultado);
         }
         

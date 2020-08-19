1d0b30ec65b39b5cb17ecb1e6afc8b0efa04734a
abigailarmijohernandez <abi1521@hotmail.com>
2020-08-18 22:14:23 -0500

    * login.data.csv: * CaducaRest.IntegrationTest.csproj: * Login.postman_collection.json: * Login.Azure.postman_environment.json:
    * test.bat: Postman

---
+++ CaducaRest.IntegrationTest/postman/test.bat
@@ -0,0 +1,5 @@
+call newman run PostmanTesting.postman_collection.json -x --reporters junit Results\PostmanJunit.xml
+call newman run test.postman_collection.json -x -r htmlextra --reporters junit --reporter-junit-export Results\junitReport2.xml 
+call newman run Login/Login.postman_collection.json -e Login/Login.Azure.postman_environment.json -d Login/login.data.csv -x -r junitfull --reporter-junitfull-export Results/junitReport3.xml -n 2
+call newman run Login/Login.postman_collection.json -e Login/Login.Azure.postman_environment.json -d Login/login.data.csv -x -r htmlextra --reporter-htmlextra-skipSensitiveData --reporter-htmlextra-showMarkdownLinks Results/report.html
+call newman run Login/Login.postman_collection.json -e Login/Login.Azure.postman_environment.json -d Login/login.data.csv -x --reporters junit Results\LoginJunit.xml

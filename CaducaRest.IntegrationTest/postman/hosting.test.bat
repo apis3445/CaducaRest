a343d90bb4764f7f0429a47752db498a8455ae8e
abigailarmijohernandez <abi1521@hotmail.com>
2022-01-22 16:54:57 -0600

    Update postman testing

--- CaducaRest.IntegrationTest/postman/hosting.test.bat
+++
@@ -1,4 +0,0 @@
-call newman run Regression.postman_collection.json -e Login/Login.Hosting.postman_environment.json -x --reporters junit Results\PostmanJunit.xml
-call newman run Login/Login.postman_collection.json -e Login/Login.Hosting.postman_environment.json -d Login/login.data.csv -x -r junitfull --reporter-junitfull-export Results/junitReport3.xml 
-call newman run Login/Login.postman_collection.json -e Login/Login.Hosting.postman_environment.json -d Login/login.data.csv -x -r htmlextra --reporter-htmlextra-skipSensitiveData --reporter-htmlextra-showMarkdownLinks Results/report.html
-call newman run Login/Login.postman_collection.json -e Login/Login.Hosting.postman_environment.json -d Login/login.data.csv -x --reporters cli,junit --reporter-junit-export Results\junitReport.xml 

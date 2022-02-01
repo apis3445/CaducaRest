8c0ac6387d1f08713c9bfc5f8419babef9c7c2fc
abigailarmijohernandez <abi1521@hotmail.com>
2022-01-31 21:47:15 -0600

    Se agregó código para subir el screenshoot

--- CaducaRest.PlayWright.UITest/PlaywrightTest.cs
+++ CaducaRest.PlayWright.UITest/PlaywrightTest.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 using Microsoft.Playwright;
 using NUnit.Framework;
 
@@ -7,6 +9,7 @@ namespace CaducaRest.PlayWright.UITest
     
     public class PlaywrightTest
     {
+        IPage page;
         [Test]
         public async Task TestGoogle()
         {
@@ -16,12 +19,20 @@ namespace CaducaRest.PlayWright.UITest
             await using var browser = await playwright.Chromium.LaunchAsync(launchOptions);
             await using var context = await browser.NewContextAsync();
 
-            var page = await context.NewPageAsync();
+            page = await context.NewPageAsync();
             await page.GotoAsync("https://www.google.com/?gl=us&hl=en");
             var searchButtonText = await page.GetAttributeAsync("input[name='btnK']","value");
             Assert.AreEqual("Google Search", searchButtonText);
-            await page.ScreenshotAsync(new PageScreenshotOptions { Path = "google.png" });
+            await TakeScreenShootAsync ("google.png");
 
+        }
+
+        public async Task TakeScreenShootAsync(string name)
+        {
+            var screenImage = Path.Combine(TestContext.CurrentContext.TestDirectory, name + "-" + Guid.NewGuid().ToString() + ".png");
+            var imageBytes = await page.ScreenshotAsync(new PageScreenshotOptions { FullPage = true });
+            File.WriteAllBytes(screenImage, imageBytes);
+            TestContext.AddTestAttachment(screenImage);
         }
     }
 }

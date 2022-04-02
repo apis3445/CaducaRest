5a49ba2724daafbd9de0b7520e97e1a24fff728d
abigailarmijohernandez <abi1521@hotmail.com>
2022-04-01 21:49:21 -0600

    Add misssing file

---
+++ CaducaRest.IntegrationTest/Model/Usuario.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace CaducaRest.IntegrationTest.Model
+{
+    public class Usuarios
+    {
+        public string Usuario { get; set; }
+        public string Nombre { get; set; }
+    }
+}

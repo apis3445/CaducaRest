6f7182f24cd962dbb88088157e722c851accf000
abigailarmijohernandez <abi1521@hotmail.com>
2023-07-14 21:27:44 -0600

    feature: update versions and add data

--- CaducaRest/Program.cs
+++ CaducaRest/Program.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System;
+using CaducaRest.Datos;
+using CaducaRest.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace CaducaRest;
 
@@ -15,7 +20,7 @@ public class Program
     public static void Main(string[] args)
     {
         var host = CreateHostBuilder(args).Build();
-
+        SeedDatabase(host);
         host.Run();
     }
 
@@ -31,4 +36,22 @@ public class Program
             {
                 webBuilder.UseStartup<Startup>();
             });
+
+    private static void SeedDatabase(IHost host)
+    {
+        using (var scope = host.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            try
+            {
+                var context = services.GetRequiredService<CaducaContext>();
+                InicializaDatos.Inicializar(context);
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex.Message);
+            }
+        }
+    }
 }

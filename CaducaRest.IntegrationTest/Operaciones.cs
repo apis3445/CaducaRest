f7fffa5f58e14dcdd5c176acbb77870c5a03684e
abigailarmijohernandez <abi1521@hotmail.com>
2023-08-21 21:30:58 -0600

    actualizar paquetes nuget

--- CaducaRest.IntegrationTest/Operaciones.cs
+++ CaducaRest.IntegrationTest/Operaciones.cs
@@ -1,4 +1,6 @@
-﻿namespace CaducaRest.IntegrationTest;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CaducaRest.IntegrationTest;
 
 /// <summary>
 /// Maneja las diferentes operacione aritméticas
@@ -8,14 +10,18 @@ public class Operaciones
     private int _a;
     private int _b;
 
-    public Operaciones(int a, int b)
+    private readonly TestContext _testContext;
+
+    public Operaciones(int a, int b, TestContext testContext)
     {
         this._a = a;
         this._b = b;
+        _testContext = testContext;
     }
 
     public int Sumar()
     {
+        _testContext.AddResultFile("");
         return _a + _b;
     }
 }
